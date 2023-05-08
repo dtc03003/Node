a9cd292cdfb5270b5d5ca69aa821f55141b241ab
김동현 <dtc03003@naver.com>
2023-05-08 19:04:22 +0900

    const,let

---
+++ ES2015+/1.const, let.js
@@ -0,0 +1,18 @@
+if (true) {
+    var x = 3;
+}
+console.log(x);
+
+if (true) {
+    const y = 3;
+    console.log(y);
+}
+// console.log(y);  // 에러 발생 - 블록 밖에서 접근
+
+const a = 0;
+// a = 1;   // 에러 발생 - 다른 값 할당
+
+let b = 0;
+b = 1;
+
+// const c;    // 에러 발생 - 값 할당 X
