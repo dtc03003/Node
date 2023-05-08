0b028b8fb69ad21aec403c9404ce0c8d2e97e005
김동현 <dtc03003@naver.com>
2023-05-08 19:09:05 +0900

    템플릿 문자열

---
+++ ES2015+/2.템플릿 문자열.js
@@ -0,0 +1,13 @@
+// 기존 ES5
+var num1 = 1;
+var num2 = 2;
+var result = 3;
+var string1 = num1 + " 더하기 " + num2 + "는 '" + result + "'";
+console.log(string1);
+
+// ES2015
+const num3 = 1;
+const num4 = 2;
+const result2 = 3;
+const string2 = `${num3} 더하기 ${num4}는 '${result2}'`;
+console.log(string2);
