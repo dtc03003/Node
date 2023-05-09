8f60b668a940a692fc7d73cbad9850fb8c397c62
김동현 <dtc03003@naver.com>
2023-05-10 07:27:02 +0900

    화살표

--- ES2015+/4.화살표 함수.js
+++ ES2015+/4.화살표 함수.js
@@ -15,3 +15,26 @@ function not1(x) {
 }
 
 const not2 = (x) => !x;
+
+var relationship1 = {
+    name: "zero",
+    friends: ["nero", "hero", "xero"],
+    logFriends: function () {
+        var that = this;
+        this.friends.forEach(function (friend) {
+            console.log(that.name, friend);
+        });
+    },
+};
+relationship1.logFriends();
+
+const relationship2 = {
+    name: "zero",
+    friends: ["nero", "hero", "xero"],
+    logFriends() {
+        this.friends.forEach((friend) => {
+            console.log(this.name, friend);
+        });
+    },
+};
+relationship2.logFriends();
