43d9231eca9dd6bd40669495eea44361057a6ad4
김동현 <dtc03003@naver.com>
2023-05-19 00:07:33 +0900

    자주 사용하는 미들웨어

--- Express/app.js
+++ Express/app.js
@@ -1,12 +1,46 @@
 const express = require("express");
+const morgan = require("morgan");
+const cookieParser = require("cookie-parser");
+const session = require("express-session");
+const dotenv = require("dotenv");
 const path = require("path");
 
+dotenv.config();
 const app = express();
-app.set("port", process.env.PORT || 3000);
+app.set("port", process.env.PORT || 3000); // process.env객체에 ROT속성이 있다면 그 값을 사용 없다면 3000번
 
-app.get("/", (req, res) => {
-    // res.send("Hello, Express");
-    res.sendFile(path.join(__dirname, "/index.html"));
+app.use(morgan("dev"));
+app.use("/", express.static(path.join(__dirname, "public")));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser(process.env.COOKIE_SECRET));
+app.use(
+    session({
+        resave: false,
+        saveUninitialized: false,
+        secret: process.env.COOKIE_SECRET,
+        cookie: {
+            httpOnly: true,
+            secure: false,
+        },
+        name: "session-cookie",
+    })
+);
+
+app.get(
+    "/",
+    (req, res, next) => {
+        console.log("GET / 요청에서만 실행");
+        next();
+    },
+    (req, res) => {
+        throw new Error("에러는 에러 처리 미들웨어로 감");
+    }
+);
+
+app.use((err, req, res, next) => {
+    console.error(err);
+    res.status(500).send(err.message);
 });
 
 app.listen(app.get("port"), () => {
