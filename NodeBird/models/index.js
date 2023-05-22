d4410024612b9ce47458312997cb8a3948ceaa72
김동현 <dtc03003@naver.com>
2023-05-22 21:06:59 +0900

    데이터베이스

--- NodeBird/models/index.js
+++ NodeBird/models/index.js
@@ -1,43 +1,33 @@
-'use strict';
+const Sequelize = require("sequelize");
+const fs = require("fs");
+const path = require("path");
+const env = process.env.NODE_ENV || "development";
+const config = require("../config/config")[env];
 
-const fs = require('fs');
-const path = require('path');
-const Sequelize = require('sequelize');
-const process = require('process');
-const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
-
-let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
-}
-
-fs
-  .readdirSync(__dirname)
-  .filter(file => {
-    return (
-      file.indexOf('.') !== 0 &&
-      file !== basename &&
-      file.slice(-3) === '.js' &&
-      file.indexOf('.test.js') === -1
-    );
-  })
-  .forEach(file => {
-    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
-    db[model.name] = model;
-  });
-
-Object.keys(db).forEach(modelName => {
-  if (db[modelName].associate) {
-    db[modelName].associate(db);
-  }
-});
+const sequelize = new Sequelize(config.database, config.username, config.password, config);
 
 db.sequelize = sequelize;
-db.Sequelize = Sequelize;
+
+const basename = path.basename(__filename);
+fs.readdirSync(__dirname) // 현재 폴더의 모든 파일을 조회
+    .filter((file) => {
+        // 숨김 파일, index.js, js 확장자가 아닌 파일 필터링
+        return file.indexOf(".") !== 0 && file !== basename && file.slice(-3) === ".js";
+    })
+    .forEach((file) => {
+        // 해당 파일의 모델 불러와서 init
+        const model = require(path.join(__dirname, file));
+        console.log(file, model.name);
+        db[model.name] = model;
+        model.initiate(sequelize);
+    });
+
+Object.keys(db).forEach((modelName) => {
+    // associate 호출
+    if (db[modelName].associate) {
+        db[modelName].associate(db);
+    }
+});
 
 module.exports = db;
