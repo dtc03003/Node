1b0364ebedb4bd13cc935be448c011358091447b
김동현 <dtc03003@naver.com>
2023-05-23 20:14:53 +0900

    이미지 업로드 구현

--- NodeBird/controllers/page.js
+++ NodeBird/controllers/page.js
@@ -1,3 +1,5 @@
+const { User, Post } = require("../models");
+
 exports.renderProfile = (req, res) => {
     res.render("profile", { title: "내 정보 - NodeBird" });
 };
@@ -12,4 +14,23 @@ exports.renderMain = (req, res, next) => {
         title: "NodeBird",
         twits,
     });
+};
+
+exports.renderMain = async (req, res, next) => {
+    try {
+        const posts = await Post.findAll({
+            include: {
+                model: User,
+                attributes: ["id", "nick"],
+            },
+            order: [["createdAt", "DESC"]],
+        });
+        res.render("main", {
+            title: "NodeBird",
+            twits: posts,
+        });
+    } catch (err) {
+        console.error(err);
+        next(err);
+    }
 };
