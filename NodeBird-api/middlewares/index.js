96fec2c4cbd92ffad79ebeda5ece222e8dcee93e
김동현 <dtc03003@naver.com>
2023-05-28 22:25:28 +0900

    JWT 토큰 추가

--- NodeBird-api/middlewares/index.js
+++ NodeBird-api/middlewares/index.js
@@ -14,3 +14,22 @@ exports.isNotLoggedIn = (req, res, next) => {
         res.redirect(`/?error=${message}`);
     }
 };
+
+exports.verifyToken = (req, res, next) => {
+    try {
+        res.locals.decoded = jwt.verify(req.headers.authorization, process.env.JWT_SECRET);
+        return next();
+    } catch (error) {
+        if (error.name === "TokenExpiredError") {
+            // 유효기간 초과
+            return res.status(419).json({
+                code: 419,
+                message: "토큰이 만료되었습니다",
+            });
+        }
+        return res.status(401).json({
+            code: 401,
+            message: "유효하지 않은 토큰입니다",
+        });
+    }
+};
