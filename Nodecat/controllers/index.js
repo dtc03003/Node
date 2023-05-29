892ab5ec569f13b594c8428381085ba07bcb5a32
김동현 <dtc03003@naver.com>
2023-05-29 20:13:50 +0900

    에러 수정

--- Nodecat/controllers/index.js
+++ Nodecat/controllers/index.js
@@ -10,19 +10,23 @@ exports.test = async (req, res, next) => {
             });
             if (tokenResult.data?.code === 200) {
                 // 토큰 발급 성공
+                console.log("토큰 저장!");
                 req.session.jwt = tokenResult.data.token; // 세션에 토큰 저장
             } else {
                 // 토큰 발급 실패
+                console.log("토큰 발급 실패!");
                 return res.json(tokenResult.data); // 발급 실패 사유 응답
             }
         }
         // 발급받은 토큰 테스트
         const result = await axios.get("http://localhost:8002/v1/test", {
             headers: { authorization: req.session.jwt },
+            withCredentials: true,
         });
         return res.json(result.data);
     } catch (error) {
         console.error(error);
+        console.log("에러발생!");
         if (error.response?.status === 419) {
             // 토큰 만료 시
             return res.json(error.response.data);
