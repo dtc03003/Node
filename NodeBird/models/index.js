8e395c506ae27d5892843805ee8ce58df565b764
김동현 <dtc03003@naver.com>
2023-06-04 20:01:02 +0900

    테스트 커버리지

--- NodeBird/models/index.js
+++ NodeBird/models/index.js
@@ -13,7 +13,12 @@ const basename = path.basename(__filename);
 fs.readdirSync(__dirname) // 현재 폴더의 모든 파일을 조회
     .filter((file) => {
         // 숨김 파일, index.js, js 확장자가 아닌 파일 필터링
-        return file.indexOf(".") !== 0 && file !== basename && file.slice(-3) === ".js";
+        return (
+            file.indexOf(".") !== 0 &&
+            !file.includes("test") &&
+            file !== basename &&
+            file.slice(-3) === ".js"
+        );
     })
     .forEach((file) => {
         // 해당 파일의 모델 불러와서 init
